cddea3ad898a88f59794fb3f827030e7ccd40a48
Ben Olson <molson5@vols.utk.edu>
2019-05-19 15:39:29 -0400

    Adding more configs

--- install-gui.sh
+++ install-gui.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-sudo pacman -Sy i3 i3status compton
+sudo pacman -Sy --needed xorg-server \
+	lightdm \
+	lightdm-gtk-greeter \
+	i3 \
+	i3status \
+	compton \
+	terminator \
+	feh \
+	chromium
+
+# Configure LightDM
+sudo systemctl enable lightdm.service
+sudo cp etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf
+
+# Configure i3
+mkdir -p ~/.config/i3
+cp .config/i3/config ~/.config/i3
+
+
